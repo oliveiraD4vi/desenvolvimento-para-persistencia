8b3b731e17ff4cf526e78ce6ec40b0f61a8203bd
Davi Oliveira <davi.comp.oliveira@gmail.com>
2022-09-12 16:44:08 -0300

    adicionada questao 1 da lista 4

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Desenvolvimento para Persistencia
+# Desenvolvimento para Persistência
 
 Este é um repositório para os exercícios desenvolvidos durante a disciplina de Desenvolvimento para Persistência.
---
+++ lista4/questao1/Movie.java
@@ -0,0 +1,60 @@
+package lista4.questao1;
+
+public class Movie {
+  private int id;
+  private String title;
+  private String director;
+  private String sinopse;
+
+  public Movie(int id, String title, String director, String sinopse) {
+    this.id = id;
+    this.title = title;
+    this.director = director;
+    this.sinopse = sinopse;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getDirector() {
+    return director;
+  }
+
+  public String getSinopse() {
+    return sinopse;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public void setDirector(String director) {
+    this.director = director;
+  }
+
+  public void setSinopse(String sinopse) {
+    this.sinopse = sinopse;
+  }
+
+  @Override
+  public String toString() {
+    StringBuffer stringBuffer = new StringBuffer();
+
+    stringBuffer.append("\nMovie ID: " + getId());
+    stringBuffer.append("\nTitle: " + getTitle());
+    stringBuffer.append("\nDirector: " + getDirector());
+    stringBuffer.append("\nSinopse: " + getSinopse());
+    stringBuffer.append("\n");
+    
+    return stringBuffer.toString();
+  }
+}
