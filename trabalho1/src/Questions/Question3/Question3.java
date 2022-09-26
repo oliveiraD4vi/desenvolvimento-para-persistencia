c2fa10f653fa398d3e99e9fab13d19cd1088f2f6
Davi Oliveira <davi.comp.oliveira@gmail.com>
2022-09-26 09:56:19 -0300

    adicionada questao 3

--- trabalho1/src/Questions/Question3/Question3.java
+++ trabalho1/src/Questions/Question3/Question3.java
@@ -1,5 +1,41 @@
 package Questions.Question3;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 public class Question3 {
-  
+  public static void main(String[] args) throws IOException {
+    String arqentry = args[0];
+    String arqoutput = args[1];
+
+    int cont;
+    byte[] data = new byte[4096];
+
+    try {
+      File file = new File(arqentry);
+      ZipEntry entry = new ZipEntry(file.getName());
+      FileInputStream inputStream = new FileInputStream(file);
+
+      FileOutputStream destiny = new FileOutputStream(new File(arqoutput));
+      BufferedInputStream origin = new BufferedInputStream(inputStream, 4096);
+      ZipOutputStream output = new ZipOutputStream(new BufferedOutputStream(destiny));
+
+      output.putNextEntry(entry);
+
+      while((cont = origin.read(data, 0, 4096)) != -1) {
+        output.write(data, 0, cont);
+      }
+
+      origin.close();
+      output.close();
+    } catch(IOException e) {
+      throw new IOException(e.getMessage());
+    }
+  }
 }
---
+++ trabalho1/src/Questions/Question5/Question5.java
@@ -0,0 +1,21 @@
+package Questions.Question5;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.xml.bind.DatatypeConverter;
+
+public class Question5 {
+  public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+    String filename = args[0];
+
+    MessageDigest sha1 = MessageDigest.getInstance("SHA1");
+    sha1.update(Files.readAllBytes(Paths.get(filename)));
+    byte[] digest = sha1.digest();
+
+    System.out.println(DatatypeConverter.printHexBinary(digest).toUpperCase());
+  }
+}
